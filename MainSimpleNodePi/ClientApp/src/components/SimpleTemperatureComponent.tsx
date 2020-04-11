8f84eaf51e8da1b75b3a28584d547d13c1590118
Michał Skóra <skorek.michal@gmail.com>
2020-04-11 12:57:41 +0200

    added temperature component - empty for now

---
+++ MainSimpleNodePi/ClientApp/src/components/SimpleTemperatureComponent.tsx
@@ -0,0 +1,7 @@
+﻿import React, { Component } from 'react';
+
+export class SimpleTemperatureComponent extends Component {
+    public render() {
+        return <p>1</p>;
+    }
+}
---
+++ MainSimpleNodePi/Controllers/TemperatureController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using MainSimpleNodePi.Sensors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MainSimpleNodePi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TemperatureController : ControllerBase
+    {
+        private readonly ITemperatureReader _temperatureReader;
+
+        public TemperatureController(ITemperatureReader temperatureReader)
+        {
+            _temperatureReader = temperatureReader;
+        }
+
+        [HttpGet]
+        public decimal Get()
+        {
+            return _temperatureReader.ReadTemperature();
+        }
+    }
+}
