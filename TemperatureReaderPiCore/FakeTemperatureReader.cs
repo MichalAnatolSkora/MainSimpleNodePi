8a67cab043390a184ad071a33e2b8d5160e7b71c
Michał Skóra <skorek.michal@gmail.com>
2020-04-13 07:18:40 +0200

    fake temperature reader returns hardcoded changing per call data

--- TemperatureReaderPiCore/FakeTemperatureReader.cs
+++ TemperatureReaderPiCore/FakeTemperatureReader.cs
@@ -4,9 +4,17 @@ namespace TemperatureReaderPiCore
 {
     public class FakeTemperatureReader : ITemperatureReader
     {
+        private readonly decimal[] _temperatures = { 15, 16, 17, 18, 19, 20, 21, 22 };
+        private int _indexToUse;
+
         public async Task<decimal> ReadTemperature()
         {
-            return await Task.FromResult(40m);
+            if (_indexToUse >= _temperatures.Length)
+            {
+                _indexToUse = 0;
+            }
+
+            return await Task.FromResult(_temperatures[_indexToUse++]);
         }
     }
 }
